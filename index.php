1458b6cde24842833d83076030338ca27195ef09
nicolas.grissi <nicolas.grissi@edenwork.com>
2018-11-28 14:09:34 +0100

    <feat> add delete all checked elements feature

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ include_once "functions.php";
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>TodoList</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <link rel="stylesheet" href="app.css">
 
 </head>
@@ -38,29 +39,29 @@ include_once "functions.php";
                     
                     <input hidden type='text' name='id' value='". $unDone['id'] ."'>
                     
-                     <button class=\"btn btn-outline-primary\" name=\"done\" type=\"submit\"  >ok</button>
+                     <button class=\"btn btn-success\" name=\"done\" type=\"submit\"  ><i class=\"fas fa-check-square\"></i></button>
                    
                     
                 </div>
 
                 <input type=\"text\" class=\"form-control\" name='todo' value=\"" . $unDone['content'] . "\"  aria-label=\"Text input with checkbox\">
                 <div class=\"input-group-append\" id=\"button-addon4\">
-                    <button class=\"btn btn-outline-primary\" name=\"edit\" type=\"submit\">Editer</button>
-                    <button class=\"btn btn-outline-danger\" name=\"delete\" type=\"submit\">Supprimer</button>
+                    <button class=\"btn btn-outline-primary \" name=\"edit\" type=\"submit\"><i class=\"fas fa-edit\"></i></button>
+                    <button class=\"btn btn-outline-danger\" name=\"delete\" type=\"submit\"><i class=\"fas fa-trash\"></i></button>
                 </div>
             </div>
                 </form>";
                 }
                 ?>
 
-            <form method="post" action="functions.php" class="mb-2">
+            <form method="post" action="functions.php" class="mb-2 mt-4">
                 <div class="input-group ">
                 <input type="text" class="form-control" name="add_content" aria-label="Text input with checkbox" placeholder="Ajouter un element">
 
                     <input type="text" hidden name="add_state" value="0">
 
                 <div class="input-group-append" id="button-addon4">
-                    <button class="btn btn-outline-success" type="submit">Ajouter</button>
+                    <button class="btn btn-success" type="submit"><i class="fas fa-plus"></i></button>
 
                 </div>
             </div>
@@ -90,7 +91,7 @@ include_once "functions.php";
 	        ?>
 
             <form method="post" action="functions.php" class="mt-4">
-              <button class="btn btn-outline-danger" name="delete_alldone" type="submit">Supprimer tous les elements cochés</button>
+              <button class="btn btn-block btn-outline-danger" name="delete_alldone" type="submit">Supprimer tous les elements cochés</button>
             </form>
 
 
